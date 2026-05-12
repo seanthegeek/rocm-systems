bf050c46d9e12d63f33ba3a9c84603db921b112f
Jatin Chaudhary <jatchaud@amd.com>
2026-05-12 14:50:05 +0100

    [clr] Add bf16 header for gcc as host compiler

--- projects/clr/hipamd/include/hip/amd_detail/amd_hip_bf16.h
+++ projects/clr/hipamd/include/hip/amd_detail/amd_hip_bf16.h
@@ -126,6 +126,7 @@
 #define HIPRT_NAN_BF16 __ushort_as_bfloat16((unsigned short)0x7FFFU)
 #define HIPRT_NEG_ZERO_BF16 __ushort_as_bfloat16((unsigned short)0x8000U)
 
+#if defined(__clang__)
 // Since we are using unsigned short to represent data in bfloat16, it can be of different sizes on
 // different machines. These naive checks should prevent some undefined behavior on systems which
 // have different sizes for basic types.
@@ -1985,4 +1986,9 @@ __BF16_DEVICE_STATIC__ __hip_bfloat16 unsafeAtomicAdd(__hip_bfloat16* address,
 }
 #endif  // defined(__clang__) && defined(__HIP__)
 #pragma pop_macro("MAYBE_UNDEF")
+#elif defined(__GNUC__) || defined(_MSC_VER)
+#if !defined(__HIPCC_RTC__)
+#include "amd_hip_bf16_gcc.h"
+#endif
+#endif
 #endif
