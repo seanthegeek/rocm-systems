fc6cc1b168ae6150995a96c3d2426aa99f6460aa
Gerardo Hernandez <gerardo.hernandez@amd.com>
2026-05-20 14:38:53 +0100

    AIRUNTIME-171 - fix GENERATE_SCAN_FUNC() for fp16 when not in cooperative_groups::impl

--- projects/clr/hipamd/include/hip/amd_detail/amd_hip_fp16.h
+++ projects/clr/hipamd/include/hip/amd_detail/amd_hip_fp16.h
@@ -1164,10 +1164,13 @@ template <typename MaskT> __device__ inline __half __reduce_max_sync(MaskT mask,
   return __reduce_op_sync(mask, val, op, wfReduce);
 }
 
+namespace cooperative_groups {
+namespace impl {
 GENERATE_SCAN_FUNC(add, f16, __half);
 GENERATE_SCAN_FUNC(min, f16, __half);
 GENERATE_SCAN_FUNC(max, f16, __half);
-
+}
+}
 #endif  // __HIP_NO_HALF_OPERATORS__
 
 #endif  // defined(__cplusplus)
