f70e74925dc20c1d8dcb7f4f5ccc4ffa00bc50a2
Carrie Fallows <Carrie.Fallows@amd.com>
2026-06-06 05:42:47 -0400

    Fix import in profile mode errors during ctest by adding benchmarking supported list that is exclusive to the roofline supported list, since the two can differ at any point in time
    Signed-off-by: Carrie Fallows <Carrie.Fallows@amd.com>

--- projects/rocprofiler-compute/src/rocprof_compute_soc/soc_base.py
+++ projects/rocprofiler-compute/src/rocprof_compute_soc/soc_base.py
@@ -15,8 +15,7 @@ from pathlib import Path
 from typing import Any, Optional
 
 import config
-from roofline.roofline_main import ROOFLINE_SUPPORTED
-from roofline.run_benchmark import run_roofline_benchmark
+from roofline.run_benchmark import BENCHMARKING_SUPPORTED, run_roofline_benchmark
 from utils import amdsmi_interface
 from utils.logger import (
     console_debug,
@@ -887,7 +886,7 @@ class OmniSoC_Base:
         # If --filter-blocks is provided, roofline block (block 4) should be mentioned
         if (
             self.get_args().no_roof
-            or self.__arch not in ROOFLINE_SUPPORTED
+            or self.__arch not in BENCHMARKING_SUPPORTED
             or (
                 self.get_args().filter_blocks
                 and "4" not in self.get_args().filter_blocks
--- projects/rocprofiler-compute/src/roofline/run_benchmark.py
+++ projects/rocprofiler-compute/src/roofline/run_benchmark.py
@@ -21,6 +21,8 @@
 import importlib
 from pathlib import Path
 
+BENCHMARKING_SUPPORTED = ["gfx90a", "gfx940", "gfx941", "gfx942", "gfx950", "gfx1151"]
+
 
 def run_roofline_benchmark(
     device_id: int, roofline_csv: Path, cache_sizes: dict
