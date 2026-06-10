cae42daedd72d458fe4906b78288fe15e57e4393
Xuan Chen <xuchen@amd.com>
2026-06-10 15:43:24 -0400

    always reference --experimental before the feature

--- projects/rocprofiler-compute/src/rocprof_compute_base.py
+++ projects/rocprofiler-compute/src/rocprof_compute_base.py
@@ -173,7 +173,7 @@ class RocProfCompute:
                         "Block 30 (Memory Bandwidth Analysis) is an experimental "
                         "feature.\n"
                         f'To use "-b {block_input}", you must also specify: '
-                        "--membw-analysis --experimental"
+                        "--experimental --membw-analysis"
                     )
             # Block 21 (PC sampling) is profile-only; analyze auto-detects it
             # from the profiling config yaml.
@@ -182,7 +182,7 @@ class RocProfCompute:
                     console_error(
                         "Block 21 (PC Sampling) is an experimental feature.\n"
                         f'To use "-b {block_input}", you must also specify: '
-                        "--pc-sampling --experimental"
+                        "--experimental --pc-sampling"
                     )
 
         # When --pc-sampling is set, inject "21" into filter_blocks so the
