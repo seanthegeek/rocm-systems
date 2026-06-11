b7a1717a97431890b752626cd24f78ec0d3a461e
kjayapra-amd <karthik.jayaprakash@amd.com>
2026-06-11 14:54:24 -0400

    Enabling fast path support for gfx1250.

--- projects/clr/hipamd/include/hip/amd_detail/amd_hip_fp8.h
+++ projects/clr/hipamd/include/hip/amd_detail/amd_hip_fp8.h
@@ -13,7 +13,7 @@
 #define _HIP_INCLUDE_HIP_AMD_DETAIL_HIP_FP8_H_
 
 #if (defined(__gfx942__) || defined(__gfx1200__) || defined(__gfx1201__) ||                        \
-     defined(__gfx950__)) &&                                                                       \
+     defined(__gfx950__)) || defined (__gfx1250__) &&                                              \
     __HIP_DEVICE_COMPILE__
 #define HIP_FP8_CVT_FAST_PATH 1
 #else
