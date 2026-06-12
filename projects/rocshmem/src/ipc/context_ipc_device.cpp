c26a3a893ab2b18b4aa5d720a869dca346e20f68
amirsojoodi <amir.sojoodi@amd.com>
2026-06-12 04:03:02 +0000

    Fix: enable acquire on IPC fence

--- projects/rocshmem/src/ipc/context_ipc_device.cpp
+++ projects/rocshmem/src/ipc/context_ipc_device.cpp
@@ -77,12 +77,13 @@ __device__ void IPCContext::getmem_nbi(void *dest, const void *source,
 }
 
 __device__ void IPCContext::fence() {
-  ipcImpl_.ipcFence();
+  ipcImpl_.ipcFence<detail::atomic::memory_scope_system,
+                    detail::atomic::memory_order_acq_rel>();
 }
 
 __device__ void IPCContext::fence(int pe) {
   ipcImpl_.ipcFence<detail::atomic::memory_scope_system,
-                    detail::atomic::memory_order_release>(pe);
+                    detail::atomic::memory_order_acq_rel>(pe);
 }
 
 __device__ void IPCContext::quiet() {
